372e26fee315ed25fa138af926726c4142a7ca3a
Babu <1058509662@qq.com>
2024-02-18 22:24:33 +0800

    #

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,9 +2,42 @@
 import "./App.css";
 // import Posts from "./components/Posts";
 
+import { useState } from "react";
+
+import { useEffect } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  function updateCount() {
+    setCount(count + 1);
+  }
+  return <button onClick={updateCount}>Count is: {count}</button>;
+}
+
+function Post(prob) {
+  return (
+    <>
+      <h4>{prob.key}</h4>
+      <p>{prob.post}</p>
+    </>
+  );
+}
+
+function PostList() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((posts) => setPosts(posts));
+  }, []);
+  return posts.map((post) => <Post key={post.id} post={post} />);
+}
+
 function App() {
   return (
     <div className="App">
+      <Counter />
+      <PostList />
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src="/Babu.svg" className="App-logo" alt="logo" />
