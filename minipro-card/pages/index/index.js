c9912eed14c51d0a3cbed2fc09494f57875df309
Babu <1058509662@qq.com>
2024-02-22 02:49:21 +0800

    minipro-card

--- minipro-card/pages/index/index.js
+++ minipro-card/pages/index/index.js
@@ -1,2 +1,30 @@
 // index.js
-Page({})
+const myaudio = wx.createInnerAudioContext();
+
+Page({
+  data: {
+    isPlay: false,
+    toName: "张三",
+    context: "祝你十连三金，欧气满满！",
+    fromName: "Babu",
+    hasSent: false,
+  },
+  onShareAppMessage() {
+    console.log("分享了");
+  },
+  onShow: function () {
+    myaudio.src = "../../statics/music.mp3";
+    console.log("展示");
+  },
+  //播放
+  play: function () {
+    myaudio.play();
+    console.log(myaudio.duration);
+    this.setData({ isPlay: true });
+  },
+  // 停止
+  stop: function () {
+    myaudio.pause();
+    this.setData({ isPlay: false });
+  },
+});
